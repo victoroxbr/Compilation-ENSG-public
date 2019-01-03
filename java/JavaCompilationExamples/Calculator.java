f07c43535780de5dc18d1224f211e09522506e80
cyann <cyann74@gmail.com>
2019-01-03 15:47:23 +0100

    calculator

--- java/JavaCompilationExamples/Calculator.java
+++ java/JavaCompilationExamples/Calculator.java
@@ -1,15 +1,11 @@
 public class Calculator {
 
-	public static void printResult(int result) {
-		System.out.println("result: " + result);
-	}
-
 	public static void main(String[] args) {
 		int a = 7;
 		int b = 8;
 		int c = a + b;
 
-		printResult(c);
+		System.out.println(c);
 	}
 
 }
