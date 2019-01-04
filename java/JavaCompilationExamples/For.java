42c601145d70ccc9c213794c4a514e41e729eea7
cyann <cyann74@gmail.com>
2019-01-04 10:01:45 +0100

    JVM and bytecode sample

---
+++ java/JavaCompilationExamples/For.java
@@ -0,0 +1,16 @@
+public class For {
+	
+	public static void print(int value) {
+		System.out.println(value);
+	}
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			print(i);
+		}
+
+
+	}
+
+}
---
+++ java/JavaCompilationExamples/If.java
@@ -0,0 +1,17 @@
+public class If {
+	
+	public static void print(int value) {
+		System.out.println(value);
+	}
+
+	public static void main(String[] args) {
+
+		int a = 7;
+		if (a > 8) {
+			print(a);
+		}
+
+
+	}
+
+}
